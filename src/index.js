5891ae957d825c293c1057303c55ef2fc0082d98
Bill Cathey <billcathey@hotmail.com>
2021-11-03 23:22:56 -0400

    Add Redux devtools, connect react with redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import "semantic-ui-css/semantic.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
