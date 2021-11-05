0b514a2660b2c50b788e6f9819cd85b64b243e65
Bill Cathey <billcathey@hotmail.com>
2021-11-05 12:01:42 -0400

    Fetch data from API with axios

--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 
 const ProductComponent = () => {
   const products = useSelector((state) => state.allProducts.products);
-  const { id, title } = products[0];
+  const { /*id,*/ title } = products[0];
   return (
     <div className="four column wide">
       <div className="ui link cards">
--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
+import axios from "axios";
 import ProductComponent from "./ProductComponent";
 
 const ProductListing = () => {
   const products = useSelector((state) => state);
-  console.log(products);
+  const fetchProducts = async () => {
+    const response = await axios
+      .get("https://fakestoreapi.com/products")
+      .catch((err) => {
+        console.log("Err", err);
+      });
+    console.log(response.data);
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
   return (
     <div className="ui grid container">
       <ProductComponent />
