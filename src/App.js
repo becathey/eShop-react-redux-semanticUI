c57aab7f4667347a41a9eb9508b8fbfab74e97de
Bill Cathey <billcathey@hotmail.com>
2021-11-05 11:37:31 -0400

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import Header from './containers/Header'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./containers/Header";
 import "./App.css";
+import ProductListing from "./containers/ProductListing";
+import ProductDetail from "./containers/ProductDetail";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductListing />} />
+          <Route path="/product/:productId" element={<ProductDetail />} />
+          <Route>404 Not Found</Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
