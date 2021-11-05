ddb27327e2916799b51a9c8e9f959b8f50b500d1
Bill Cathey <billcathey@hotmail.com>
2021-11-04 23:01:56 -0400

    Create basic structure for components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Header from './containers/Header'
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Header />
     </div>
   );
 }
